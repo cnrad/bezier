4df66f918b91279f20c9d68c171beaac63e2b686
Conrad Crawford <conradwcrawford@gmail.com>
2022-05-17 11:16:42 -0400

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+    purge: ["./src/**/*.{js,ts,jsx,tsx}"],
+    darkMode: true, // or 'media' or 'class'
+    theme: {
+        extend: {},
+    },
+    variants: {
+        extend: {},
+    },
+    plugins: [],
+};
