8d70de03b3b03aebe6bf3f8d96b003f3d82868d9
cnrad <conradwcrawford@gmail.com>
2022-05-17 20:59:51 -0400

    drag points

--- src/components/BezierCanvas.tsx
+++ src/components/BezierCanvas.tsx
@@ -7,22 +7,62 @@ export const BezierCanvas = () => {
     const [animating, setAnimating] = useState<boolean>(false);
     let timer: any = null;
 
+    const point1ref = useRef(null);
+
+    let points: any = [];
+    let pointsIn = [0, 0, 0.5, 0.8, 1, 0];
+
+    let box = { left: 0, top: 0, right: 400, bottom: 400 };
+
+    // read points from array.
+    for (let i = 0; i < pointsIn.length; i++) {
+        let x = box.left + (box.right - box.left) * pointsIn[i];
+        let y = box.bottom + (box.top - box.bottom) * pointsIn[++i];
+        points.push({ x: x, y: y });
+    }
+
+    function setPointCoords(point: any, i: number) {
+        point.setAttribute("cx", points[i].x);
+        point.setAttribute("cy", points[i].y);
+    }
+
+    const movePoint = (point: any, i: number) => {
+        document.onmousemove = function (e) {
+            console.log(e.pageX, e.pageY, e.offsetX, e.offsetY);
+            let x = e.offsetX,
+                y = e.offsetY;
+
+            // constrain withing the box
+            if (x < box.left) x = box.left;
+            if (x > box.right) x = box.right;
+            if (y > box.bottom) y = box.bottom;
+            if (y < box.top) y = box.top;
+
+            points[i].x = x;
+            points[i].y = y;
+            setPointCoords(point, i);
+            // drawPath();
+        };
+        document.onmouseup = function () {
+            document.onmousemove = document.onmouseup = null;
+        };
+        return false;
+    };
+
     useEffect(() => {
         setT(0);
+
+        timer = setInterval(() => {
+            setT(t => t + 0.01);
+            if (t >= 1) {
+                setAnimating(false);
+            }
+        }, 30);
+
         if (!animating) {
             clearInterval(timer);
             timer = null;
         }
-
-        timer = setInterval(
-            () => {
-                setT(t => t + 0.01);
-                if (t >= 1) {
-                    setAnimating(false);
-                }
-            },
-            animating ? 30 : 1000000
-        );
 
         return () => clearInterval(timer);
     }, [animating]);
@@ -37,7 +77,7 @@ export const BezierCanvas = () => {
                 width="400"
                 height="400"
                 viewBox="0 0 400 400"
-                className="bg-gray-900/50 border border-gray-500/20 text-white flex items-center justify-center rounded-md mt-7"
+                className="bg-gray-900/50 border border-gray-500/20 text-white flex items-center justify-center rounded-md"
             >
                 <defs xmlns="http://www.w3.org/2000/svg">
                     <pattern id="grid" width="20" height="20" patternUnits="userSpaceOnUse">
@@ -47,8 +87,42 @@ export const BezierCanvas = () => {
                 <rect xmlns="http://www.w3.org/2000/svg" width="100%" height="100%" fill="url(#grid)" />
 
                 <svg xmlns="http://www.w3.org/2000/svg" x={t * 300} y="169">
-                    <circle r="6" fill="white" stroke="#E8C48E" strokeWidth="1" cx="20" cy="20" />
+                    <text x="15" y="12" style={{ fill: "#fff", fontSize: "0.9rem", userSelect: "none" }}>
+                        3
+                    </text>
+                    <motion.circle
+                        r="6"
+                        fill="white"
+                        stroke="#E8C48E"
+                        strokeWidth="1"
+                        cx="20"
+                        cy="20"
+                        style={{ cursor: "pointer" }}
+                    />
                 </svg>
+
+                <path
+                    xmlns="http://www.w3.org/2000/svg"
+                    stroke="red"
+                    fill="none"
+                    id="bezier-path"
+                    stroke-width="1.2"
+                    d="M20,350 Q159,50 320,350 "
+                />
+
+                {/* <motion.svg xmlns="http://www.w3.org/2000/svg" x="-15" y="-15"> */}
+                <motion.circle
+                    r="6"
+                    fill="white"
+                    stroke="#E8C48E"
+                    strokeWidth="1"
+                    cx="20"
+                    cy="20"
+                    style={{ cursor: "pointer" }}
+                    ref={point1ref}
+                    onMouseDown={(e: any) => movePoint(e.target, 1)}
+                />
+                {/* </motion.svg> */}
             </svg>
         </>
     );
