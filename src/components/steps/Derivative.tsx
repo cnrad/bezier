d75d2b6b18702572ece47744e4303b31517d38b5
cnrad <conradwcrawford@gmail.com>
2022-05-26 22:17:01 -0400

    start derivative page

---
+++ src/components/steps/Derivative.tsx
@@ -0,0 +1,19 @@
+import { motion } from "framer-motion";
+import type { NextPage } from "next";
+import Image from "next/image";
+
+const Derivative = (props: any) => {
+    return (
+        <motion.div {...props}>
+            <h1 className="mb-4 font-black">Derivative of a Bézier curve</h1>
+
+            <p className="text-base font-normal mb-4">
+                The graph of the derivative of a Bézier curve is usually referred to as the <b>hodograph</b> of the
+                original curve. The derivative always has one less control point then the original, making the degree{" "}
+                <i>n-1</i> from the original.
+            </p>
+        </motion.div>
+    );
+};
+
+export default Derivative;
